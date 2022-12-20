bfe1aa9baa432db68f2f9614b47ec576ff03b271
Antonio Eleuterio <ampeleuterio@gmail.com>
2022-12-21 00:12:42 +0100

    Basic rest request added

---
+++ script.js
@@ -0,0 +1,25 @@
+(function() {
+    "use strict";
+
+    var app = angular.module("voViewer", []);
+    
+    
+    var FrameController = function($scope, $http) {
+        $scope.tableData = [
+            { name: 'Alice', age: 25, city: 'New York' },
+            { name: 'Bob', age: 30, city: 'Chicago' },
+            { name: 'Charlie', age: 35, city: 'San Francisco' }
+        ];
+        
+        $scope.generate = function() {
+            var promise = $http.get("https:localhost/square/6");
+
+            promise.then(function(response) {
+                pixelsGenerated = response.data;
+                $scope.tableData[0]['age'] = response.data['square']
+            });
+        };
+    }
+
+    app.controller("FrameController", ["$scope", "$http", FrameController]);
+}());
