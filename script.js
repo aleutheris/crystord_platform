f604d2e55b5c198cf3f75fcdedad3ee56876e54f
António Eleutério <ampeleuterio@gmail.com>
2023-03-20 08:10:52 +0100

    Script to update server nginx with the index and config file added

--- script.js
+++ script.js
@@ -2,15 +2,14 @@
     "use strict";
 
     var app = angular.module("voViewer", []);
-    
-    
+
     var FrameController = function($scope, $http) {
         $scope.tableData = [
-            { name: 'Alice', age: 25, city: 'New York' },
+            { name: 'Alice', age: 27, city: 'New York' },
             { name: 'Bob', age: 30, city: 'Chicago' },
             { name: 'Charlie', age: 35, city: 'San Francisco' }
         ];
-        
+
         $scope.generate = function() {
             var promise = $http.get("https:localhost/square/6");
 
