1a415f94b8c7e83488c20104120a4fc1d08f3984
António Eleutério <ampeleuterio@gmail.com>
2023-03-26 09:31:35 +0200

    Using simple calculations representation

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
-(function() {
-    "use strict";
+(function () {
+  "use strict";
 
-    var app = angular.module("voViewer", []);
+  var app = angular.module("pyApp", []);
 
-    var FrameController = function($scope, $http) {
-        $scope.tableData = [
-            { name: 'Alice', age: 27, city: 'New York' },
-            { name: 'Bob', age: 30, city: 'Chicago' },
-            { name: 'Charlie', age: 35, city: 'San Francisco' }
-        ];
+  var pyController = function ($scope, $http) {
+    const server_url = "http://localhost:5000";
 
-        $scope.generate = function() {
-            var promise = $http.get("https:localhost/square/6");
+    $scope.calculate = function () {
+      const url_add = `${server_url}/add/${$scope.number1}/${$scope.number2}`;
+      const url_sub = `${server_url}/sub/${$scope.number1}/${$scope.number2}`;
+      const url_mul = `${server_url}/mul/${$scope.number1}/${$scope.number2}`;
+      const url_div = `${server_url}/div/${$scope.number1}/${$scope.number2}`;
+      const url_exp = `${server_url}/exp/${$scope.number1}/${$scope.number2}`;
 
-            promise.then(function(response) {
-                pixelsGenerated = response.data;
-                $scope.tableData[0]['age'] = response.data['square']
-            });
-        };
-    }
+      $scope.calcTable = [
+        { add: 0, sub: 0, mul: 0, div: 0, exp: 0 }
+      ];
 
-    app.controller("FrameController", ["$scope", "$http", FrameController]);
-}());
+      $http.get(url_add)
+        .then(function(response) {
+          $scope.calcTable[0].add = response.data.result;
+        }, function(error) {
+          console.log(error);
+        });
+
+      $http.get(url_sub)
+        .then(function(response) {
+          $scope.calcTable[0].sub = response.data.result;
+        }
+        , function(error) {
+          console.log(error);
+        });
+
+      $http.get(url_mul)
+        .then(function(response) {
+          $scope.calcTable[0].mul = response.data.result;
+        }
+        , function(error) {
+          console.log(error);
+        });
+
+      $http.get(url_div)
+        .then(function(response) {
+          $scope.calcTable[0].div = response.data.result;
+        }
+        , function(error) {
+          console.log(error);
+        }
+      );
+
+      $http.get(url_exp)
+        .then(function(response) {
+          $scope.calcTable[0].exp = response.data.result;
+        }
+        , function(error) {
+          console.log(error);
+        }
+      );
+    };
+  };
+
+  app.controller("pyController", ["$scope", "$http", pyController]);
+})();
