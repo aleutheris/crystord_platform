8f018e914b72e250922010dc9d236376f3a77740
António Eleutério <ampeleuterio@gmail.com>
2023-05-14 08:27:29 +0200

    Ready to be a frontend isolated

--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-
-config_src_path = 'nginx.conf'
-config_dst_path = '/etc/nginx'
-
-os.system("sudo cp {} {}".format(config_src_path, config_dst_path))
-os.system("sudo systemctl reload nginx")
--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var app = angular.module("pyApp", []);
 
   var pyController = function ($scope, $http) {
-    const server_url = "http://localhost:5000";
+    const server_url = `https://pyapp-3j2xlemvha-ew.a.run.app`;
 
     $scope.create_unitbase = function () {
       const url = `${server_url}/api/create_unitbase`;
@@ -45,6 +45,70 @@
         }, function(error) {
           console.log(error);
         });
+    };
+
+    $scope.calculate = function () {
+      const url_host = server_url;
+
+      const url_add = `${server_url}/add/${$scope.number1}/${$scope.number2}`;
+      const url_sub = `${server_url}/sub/${$scope.number1}/${$scope.number2}`;
+      const url_mul = `${server_url}/mul/${$scope.number1}/${$scope.number2}`;
+      const url_div = `${server_url}/div/${$scope.number1}/${$scope.number2}`;
+      const url_exp = `${server_url}/exp/${$scope.number1}/${$scope.number2}`;
+
+      $scope.calcTable = [
+        { add: 0, sub: 0, mul: 0, div: 0, exp: 0 }
+      ];
+
+      $http.get(url_add)
+        .then(function(response) {
+          $scope.calcTable[0].add = response.data.result;
+        }, function(error) {
+          console.log(error);
+        });
+
+      $http.get(url_sub)
+        .then(function(response) {
+          $scope.calcTable[0].sub = response.data.result;
+        }
+        , function(error) {
+          console.log(error);
+        });
+
+      $http.get(url_mul)
+        .then(function(response) {
+          $scope.calcTable[0].mul = response.data.result;
+        }
+        , function(error) {
+          console.log(error);
+        });
+
+      $http.get(url_div)
+        .then(function(response) {
+          $scope.calcTable[0].div = response.data.result;
+        }
+        , function(error) {
+          console.log(error);
+        }
+      );
+
+      $http.get(url_exp)
+        .then(function(response) {
+          $scope.calcTable[0].exp = response.data.result;
+        }
+        , function(error) {
+          console.log(error);
+        }
+      );
+
+      $http.get(url_host)
+        .then(function(response) {
+          $scope.hostname = response.data;
+        }
+        , function(error) {
+          console.log(error);
+        }
+      );
     };
   };
 
