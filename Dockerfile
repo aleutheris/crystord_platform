4b57820c93be0d80ba2d77e66c8239c49a724554
Aleutheris <ampeleuterio@gmail.com>
2024-05-15 17:04:23 +0200

    Link made from the web to the api

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,7 @@
-# Use a base image with a web server installed (e.g., nginx)
 FROM nginx:latest
 
-# Copy the custom Nginx configuration
 COPY nginx-config/default.conf /etc/nginx/conf.d/default.conf
 
-# Copy your webpage files to the appropriate location in the container
 COPY webpage/ /usr/share/nginx/html
 
-# Expose port 80 (default for HTTP)
 EXPOSE 80
--- webpage/script.js
+++ webpage/script.js
@@ -4,121 +4,17 @@
   var app = angular.module("pyApp", []);
 
   var pyController = function ($scope, $http) {
-    const server_url = `https://pyapp-3j2xlemvha-ew.a.run.app`;
+    const server_url = `http://crystord:5000`;
 
     $scope.create_unitbase = function () {
-      const url = `${server_url}/api/create_unitbase`;
+      const url = `${server_url}/`;
       $http.get(url)
         .then(function(response) {
           console.log(response);
+          $scope.r = response.data.message;
         }, function(error) {
           console.log(error);
         });
-    };
-
-    $scope.add_unit = function () {
-      const url = `${server_url}/api/add_unit?title=${$scope.unit_title}`;
-      $http.get(url)
-        .then(function(response) {
-          console.log(response);
-        }, function(error) {
-          console.log(error);
-        });
-    };
-
-    $scope.remove_unit = function () {
-      const url = `${server_url}/api/remove_unit?unit_id=${$scope.unit_id}`;
-      $http.get(url)
-        .then(function(response) {
-          console.log(response);
-        }, function(error) {
-          console.log(error);
-        });
-    };
-
-    $scope.search_units = function () {
-      const url = `${server_url}/api/search_units`;
-
-      $http.get(url)
-        .then(function(response) {
-          $scope.search_table = response.data;
-        }, function(error) {
-          console.log(error);
-        });
-    };
-
-    $scope.calculate = function () {
-      const url_host = server_url;
-
-      const url_add = `${server_url}/add/${$scope.number1}/${$scope.number2}`;
-      const url_sub = `${server_url}/sub/${$scope.number1}/${$scope.number2}`;
-      const url_mul = `${server_url}/mul/${$scope.number1}/${$scope.number2}`;
-      const url_div = `${server_url}/div/${$scope.number1}/${$scope.number2}`;
-      const url_exp = `${server_url}/exp/${$scope.number1}/${$scope.number2}`;
-      const url_title = `${server_url}/get_title/0`;
-
-      $scope.calcTable = [
-        { add: 0, sub: 0, mul: 0, div: 0, exp: 0, title: "" }
-      ];
-
-      $http.get(url_add)
-        .then(function(response) {
-          $scope.calcTable[0].add = response.data.result;
-        }, function(error) {
-          console.log(error);
-        });
-
-      $http.get(url_sub)
-        .then(function(response) {
-          $scope.calcTable[0].sub = response.data.result;
-        }
-        , function(error) {
-          console.log(error);
-        });
-
-      $http.get(url_mul)
-        .then(function(response) {
-          $scope.calcTable[0].mul = response.data.result;
-        }
-        , function(error) {
-          console.log(error);
-        });
-
-      $http.get(url_div)
-        .then(function(response) {
-          $scope.calcTable[0].div = response.data.result;
-        }
-        , function(error) {
-          console.log(error);
-        }
-      );
-
-      $http.get(url_exp)
-        .then(function(response) {
-          $scope.calcTable[0].exp = response.data.result;
-        }
-        , function(error) {
-          console.log(error);
-        }
-      );
-
-      $http.get(url_title)
-        .then(function(response) {
-          $scope.calcTable[0].title = response.data.result;
-        }
-        , function(error) {
-          console.log(error);
-        }
-      );
-
-      $http.get(url_host)
-        .then(function(response) {
-          $scope.hostname = response.data;
-        }
-        , function(error) {
-          console.log(error);
-        }
-      );
     };
   };
 
