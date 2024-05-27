0f3871d7e12a3bb48388842a996eae8c20c1bdbe
Aleutheris <ampeleuterio@gmail.com>
2024-05-27 09:10:52 +0200

    Separated controllers for create and get element, respectively

--- webpage/script.js
+++ webpage/script.js
@@ -3,8 +3,19 @@
 
   var app = angular.module("pyApp", []);
 
-  var pyController = function ($scope, $http) {
+  app.controller("CreateElement", function ($scope, $http) {
     const server_url = `http://aleutherisnl1.synology.me:5000`;
+
+    $scope.update_element_content = function () {
+      const url = `${server_url}/api/update_element_content`;
+
+      $http.put(url, {uuid: $scope.element_id, content: $scope.element_content})
+        .then(function(response) {
+          console.log(response);
+        }, function(error) {
+          console.log(error);
+        });
+    };
 
     $scope.create_element = function () {
       const url_ce = `${server_url}/api/create_element`;
@@ -14,17 +25,15 @@
           $scope.element_id = response_ce.data.result;
           const url_ue = `${server_url}/api/update_element_content`;
 
-          $http.put(url_ue, {uuid: $scope.element_id, content: $scope.element_content})
-            .then(function(response_ue) {
-              console.log(response_ue);
-            }, function(error) {
-              console.log(error);
-            });
-
+          $scope.update_element_content();
         }, function(error) {
           console.log(error);
         });
     };
+  });
+
+  app.controller("GetElement", function ($scope, $http) {
+    const server_url = `http://aleutherisnl1.synology.me:5000`;
 
     $scope.get_element_content = function () {
       const url = `${server_url}/api/get_element_content/${$scope.element_id}`;
@@ -36,7 +45,7 @@
           console.log(error);
         });
     };
-  };
+  });
 
-  app.controller("pyController", ["$scope", "$http", pyController]);
+  // app.controller("CreateElement", ["$scope", "$http", CreateElement]);
 })();
