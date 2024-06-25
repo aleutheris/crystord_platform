385517c96612cc35211cb678cc6a19831f9e0c1b
Aleutheris <ampeleuterio@gmail.com>
2024-06-25 10:36:43 +0200

    Ability to show labels using the Get Element button.

--- webpage/script.js
+++ webpage/script.js
@@ -2,7 +2,8 @@
   "use strict";
 
   var app = angular.module("pyApp", ['ngRoute']);
-  var shared_url = 'https://aleutherisnl1.synology.me:5665/api/get_element_content/'
+  var api_url = 'https://aleutherisnl1.synology.me:5665/api/get_element_content/'
+  var share_url = 'http://crystord/element.html?element_id='
 
   app.config(['$locationProvider', function($locationProvider) {
     $locationProvider.html5Mode(true);
@@ -11,7 +12,7 @@
   app.controller("CreateElement", function ($scope, $http) {
     const server_url = `https://aleutherisnl1.synology.me:5665`;
     $scope.element_id = 'None'
-    $scope.element_content_api_url = 'None'
+    $scope.element_share_url = 'None'
 
     $scope.update_element_content = function () {
       const url = `${server_url}/api/update_element_content`;
@@ -26,11 +27,13 @@
 
     $scope.create_element = function () {
       const url = `${server_url}/api/create_element`;
+      $scope.element_id = 'None'
+      $scope.element_share_url = 'None'
 
       $http.post(url, {title: $scope.element_title, labels: [$scope.element_labels]})
         .then(function(response_ce) {
           $scope.element_id = response_ce.data.result;
-          $scope.element_content_api_url = shared_url + $scope.element_id;
+          $scope.element_share_url = share_url + $scope.element_id;
 
           $scope.update_element_content();
         }, function(error) {
@@ -42,12 +45,30 @@
   app.controller("GetElement", function ($scope, $http) {
     const server_url = `https://aleutherisnl1.synology.me:5665`;
 
+    $scope.get_element = function () {
+      $scope.get_element_content();
+      $scope.list_element_labels();
+    };
+
     $scope.get_element_content = function () {
       const url = `${server_url}/api/get_element_content/${$scope.element_id}`;
+      $scope.element_share_url = share_url + $scope.element_id;
 
       $http.get(url)
         .then(function(response) {
           $scope.element_content = response.data.result;
+        }, function(error) {
+          console.log(error);
+        });
+    };
+
+    $scope.list_element_labels = function () {
+      const url = `${server_url}/api/list_labels/${$scope.element_id}`;
+      $scope.element_share_url = share_url + $scope.element_id;
+
+      $http.get(url)
+        .then(function(response) {
+          $scope.element_labels = response.data.result;
         }, function(error) {
           console.log(error);
         });
@@ -75,12 +96,13 @@
     $scope.element_id = $location.search().element_id;
 
     const url = `${server_url}/api/get_element_all_fields/${$scope.element_id}`;
-    $scope.element_content_api_url = shared_url + $scope.element_id;
+    $scope.element_content_api_url = api_url + $scope.element_id;
 
     $http.get(url)
       .then(function(response) {
         $scope.element_data = response.data;
         $scope.element_title = response.data['Title'];
+        // $scope.element_labels = response.data['Labels'];
         $scope.element_content = response.data['Content'];
       }, function(error) {
         console.log(error);
