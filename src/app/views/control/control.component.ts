18c17ba72fca79085de76e6f0fa9ff818528fff4
Aleutheris <ampeleuterio@gmail.com>
2024-07-06 12:04:59 +0200

    Control component is able to use input group code (text boxes)

--- src/app/views/control/control.component.ts
+++ src/app/views/control/control.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { DocsExampleComponent } from '@docs-components/public-api';
+import { RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, InputGroupComponent, InputGroupTextDirective, FormControlDirective, FormLabelDirective, FormCheckInputDirective, ButtonDirective, ThemeDirective, DropdownComponent, DropdownToggleDirective, DropdownMenuDirective, DropdownItemDirective, DropdownDividerDirective, FormSelectDirective } from '@coreui/angular';
 
 @Component({
-  selector: 'app-control',
-  standalone: true,
-  imports: [],
-  templateUrl: './control.component.html',
-  styleUrl: './control.component.scss'
+    selector: 'app-control',
+    templateUrl: './control.component.html',
+    styleUrls: ['./control.component.scss'],
+    standalone: true,
+    imports: [RowComponent, ColComponent, TextColorDirective, CardComponent, CardHeaderComponent, CardBodyComponent, DocsExampleComponent, InputGroupComponent, InputGroupTextDirective, FormControlDirective, FormLabelDirective, FormCheckInputDirective, ButtonDirective, ThemeDirective, DropdownComponent, DropdownToggleDirective, DropdownMenuDirective, DropdownItemDirective, RouterLink, DropdownDividerDirective, FormSelectDirective, ReactiveFormsModule]
 })
 export class ControlComponent {
+
+  constructor() { }
 
 }
