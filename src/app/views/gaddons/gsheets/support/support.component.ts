a486366d218cd3fb25b1568efaa2252046fabde7
António Eleutério <aleutheris@gmail.com>
2025-11-17 10:11:25 +0100

    Google Sheets Add-on Privacy Policy Ready

--- src/app/views/gaddons/gsheets/support/support.component.ts
+++ src/app/views/gaddons/gsheets/support/support.component.ts
@@ -1,41 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
-import {
-  ButtonDirective,
-  CardBodyComponent,
-  CardComponent,
-  ColComponent,
-  ContainerComponent,
-  RowComponent
-} from '@coreui/angular';
+import { ButtonDirective } from '@coreui/angular';
 import { IconDirective } from '@coreui/icons-angular';
 
 @Component({
   selector: 'app-support',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterLink,
-    ButtonDirective,
-    CardBodyComponent,
-    CardComponent,
-    ColComponent,
-    ContainerComponent,
-    RowComponent,
-    IconDirective
-  ],
+  imports: [CommonModule, ButtonDirective, IconDirective],
   templateUrl: './support.component.html',
   styleUrls: ['./support.component.scss']
 })
 export class SupportComponent {
+
   constructor(private router: Router) {}
 
   goBack(): void {
-    this.router.navigate(['/sheets-addon']);
-  }
-
-  contactSupport(): void {
-    window.open('mailto:aleutheris@gmail.com?subject=Google Sheets Add-on Support', '_blank');
+    this.router.navigate(['/gaddons/sheets-addon']);
   }
 }
--- src/app/views/gaddons/gsheets/terms/terms.component.ts
+++ src/app/views/gaddons/gsheets/terms/terms.component.ts
@@ -1,36 +1,21 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import {
-  ButtonDirective,
-  CardBodyComponent,
-  CardComponent,
-  ColComponent,
-  ContainerComponent,
-  RowComponent
-} from '@coreui/angular';
+import { CommonModule } from '@angular/common';
+import { ButtonDirective } from '@coreui/angular';
 import { IconDirective } from '@coreui/icons-angular';
 
 @Component({
   selector: 'app-terms',
   standalone: true,
-  imports: [
-    CommonModule,
-    ButtonDirective,
-    CardBodyComponent,
-    CardComponent,
-    ColComponent,
-    ContainerComponent,
-    RowComponent,
-    IconDirective
-  ],
+  imports: [CommonModule, ButtonDirective, IconDirective],
   templateUrl: './terms.component.html',
   styleUrls: ['./terms.component.scss']
 })
 export class TermsComponent {
+
   constructor(private router: Router) {}
 
   goBack(): void {
-    this.router.navigate(['/sheets-addon']);
+    this.router.navigate(['/gaddons/sheets-addon']);
   }
 }
